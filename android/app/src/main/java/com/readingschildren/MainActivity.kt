0cb2fcbb28f0edefee7938d1d5650f1f067d9aa3
JhohellsDL <jhohellserick@gmail.com>
2026-03-15 13:19:55 -0500

    chore: update dependencies and enhance MainActivity with onCreate method

--- android/app/src/main/java/com/readingschildren/MainActivity.kt
+++ android/app/src/main/java/com/readingschildren/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.readingschildren
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -7,10 +8,15 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
   override fun getMainComponentName(): String = "readingsChildren"
 
   /**
